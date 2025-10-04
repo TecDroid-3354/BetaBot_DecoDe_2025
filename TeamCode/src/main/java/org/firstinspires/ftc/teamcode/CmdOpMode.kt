28359b43ae3bf397a379a7b647f7714bd16d33c5
Santiago Fernando Murguia <santiagofernandomurguia@gmail.com>
2025-10-04 13:14:35 -0600

    tested and working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CmdOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CmdOpMode.kt
@@ -1,17 +1,21 @@
 package org.firstinspires.ftc.teamcode
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.ColorSensor
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor
 import com.seattlesolvers.solverslib.command.CommandOpMode
 import com.seattlesolvers.solverslib.kinematics.wpilibkinematics.ChassisSpeeds
+import org.firstinspires.ftc.teamcode.sensor.ColorSensorEx
 import org.firstinspires.ftc.teamcode.subsystems.drivetrain.Mecanum
-import kotlin.math.PI
 import kotlin.math.pow
 
 @TeleOp(name = "CMD", group = "Op Mode")
 class CMDOpMode : CommandOpMode() {
     lateinit var mecanum: Mecanum
+    lateinit var colorSensorEx: ColorSensorEx
     override fun initialize() {
         mecanum = Mecanum(hardwareMap, telemetry)
+        colorSensorEx = ColorSensorEx(hardwareMap.get(ColorSensor::class.java, "colorSensor"), telemetry)
 
         configureButtonBindings()
 
@@ -22,15 +26,13 @@ class CMDOpMode : CommandOpMode() {
     }
 
     fun periodic() {
+
         val speeds = ChassisSpeeds( -gamepad1.left_stick_y.toDouble().pow(3.0) * 1.5,
             -gamepad1.left_stick_x.toDouble().pow(3.0) * 2.0,
             -gamepad1.right_stick_x.toDouble().pow(3.0) * 20.0)
         mecanum.setChassisSpeeds(speeds)
 
-        telemetry.addData("frontRight", mecanum.frontRightMotor.getLinearVelocity().mps)
-        telemetry.addData("frontLeft", mecanum.frontLeftMotor.getLinearVelocity().mps)
-        telemetry.addData("backRight", mecanum.backRightMotor.getLinearVelocity().mps)
-        telemetry.addData("backLeft", mecanum.backLeftMotor.getLinearVelocity().mps)
+        telemetry.addData("Detected Color", colorSensorEx.colorFromSensor)
 
         telemetry.update()
     }
